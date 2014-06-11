0ec73a140e524e03b313653230518513f7820cdc
Paweł Tajs <paweltajs@gmail.com>
2014-06-11 13:40:09 +0200

    Poprawa formatowania

--- check_ver1.cpp
+++ check_ver1.cpp
@@ -10,6 +10,9 @@
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 
+
+// g++ -Wall check_ver4.cpp -lmysqlcppconn -std=c++11
+
 char *serIRK, *usrIRK, *pwdIRK, *dbIRK;
 
 using namespace std;
@@ -45,25 +48,19 @@ void LoadDataConn(const char *_NameTxt) 	/*wczytuje dane logowania do servera z 
 
 class MyException
 {
-   private: 
-      string  msg;
-   public:
-      MyException(string str)
-      {
-         msg = str;
-      }
-      void printerrmsg()
-      {
-         cout<<msg<<endl;
-      }
+private: 
+	string  msg;
+public:
+	MyException(string str)
+	{
+		msg = str;
+	}
+	void printerrmsg()
+	{
+		cout<<msg<<endl;
+	}
 };
 
-
-bool FormatCheck(string kod){
-	return !kod.empty() && (
-	 (kod.size()==6 && isdigit(kod[0]) && isdigit(kod[1]) && kod[2]=='-' && isdigit(kod[3]) && isdigit(kod[4]) && isdigit(kod[5]))
-	 || (kod.size()==5 && isdigit(kod[0]) && isdigit(kod[1]) && isdigit(kod[2]) && isdigit(kod[3]) && isdigit(kod[4])));
-}
 
 class BadRecord{
 
@@ -72,13 +69,19 @@ class BadRecord{
 	string miejscowosc_;
 	string poczta_;
 	int err_;
+	string remarks_;
 
-	public:
+public:
 
-	BadRecord (int pk, string kod, string miejscowosc="brak", string poczta="brak"):
-		pk_(pk), kod_(kod), miejscowosc_(miejscowosc), poczta_(poczta), err_(0){
-		if(!FormatCheck(kod)) err_ = 1;
-		else err_=0;
+	BadRecord (int pk, string kod, string miejscowosc="brak", string poczta="brak")
+		:pk_(pk), kod_(kod), miejscowosc_(miejscowosc), poczta_(poczta){
+		if (kod.empty()) err_ = 11;
+		else if(kod.size()==5 && isdigit(kod[0]) && isdigit(kod[1]) && isdigit(kod[2]) && isdigit(kod[3]) && isdigit(kod[4])){
+			err_ = 0;
+			remarks_ = "[brak kreski w kodzie]";
+		}
+		else if(kod.size()==6 && isdigit(kod[0]) && isdigit(kod[1]) && kod[2]=='-' && isdigit(kod[3]) && isdigit(kod[4]) && isdigit(kod[5])) err_ = 0;
+		else err_ = 10;
 	}
 	
 	int Pk()const{
@@ -113,10 +116,11 @@ class BadRecord{
 
 		switch(err_){
 			case 0: return "OK";
-			case 1: return "błąd: zły format kodu";
+			case 10: return "błąd: zły format kodu";
 			case 2: return "błąd: kod nie istnieje";
 			case 3: return "błąd: do tego kodu nie jest przypisana podana poczta.";
 			case 4: return "ostrzeżenie: poczta pasująca do podanego kodu pocztowego jest wpisana w polu miejscowość.";
+			case 11: return "błąd: brak wpisanego kodu";
 		}
 		assert(false);
 		return "";
@@ -129,127 +133,129 @@ class BadRecord{
 	void InsertError(int er){
 		err_ = er;
 	}
+
+	void InsertRemark(string remark){
+		remarks_ = remarks_.empty() ? remark : remarks_ + " " + remark;
+	}
+
+	string Remarks() const{
+		return remarks_;
+	}
 };
 
 
 ostream& operator << (ostream& wy, BadRecord x){
-    wy << x.Pk()<< ". " ;
-	if(!x.Kod().empty()) wy << x.Kod() << " ";
-	else wy << "??????" << " ";
+	wy << x.Pk()<< ";"<< x.Kod() << ";"<< x.Miejscowosc() << ";" << x.Poczta() << ";";
 
-	if(!x.Miejscowosc().empty()) wy << x.Miejscowosc() << " ";
-	else wy << "??????" << " ";
+	if(x.IsErr()) wy<<"("  << x.Error() << " [" << x.NrErr() << "]" << ");";
+	else wy << "OK;";
 
-	if(!x.Poczta().empty()) wy << x.Poczta() << " ";
-	else wy << "??????" << " ";
-
-	if(x.IsErr()) wy<<"("  << x.Error() << " [" << x.NrErr() << "]" << ")";
-	else wy << "OK";
-	
+	if(!x.Remarks().empty()) wy << x.Remarks();
 	return wy;
 }
 
 bool SameString (string a, string b){
-	if(a.size()<1 || b.size()<1) return a==b;
-	int i = a.find("-");
-	int j = b.find("-");
-	
-	if (i!=static_cast<int>(string::npos))
-		a.erase(i);
-
-	if (j!=static_cast<int>(string::npos))
-		b.erase(j);
-
-	i = a.find(" ");
-	j = b.find(" ");
-
-	if (i!=static_cast<int>(string::npos))
-		a.erase(i);
-	if (j!=static_cast<int>(string::npos))
-		b.erase(j);
-
-	transform(a.begin(), a.end(), a.begin(), ::tolower);
-	transform(b.begin(), b.end(), b.begin(), ::tolower);
-
+	transform(a.begin(), a.end(), a.begin(), [](char x){return isalnum(x) ? std::tolower(x) : 0;});
+	transform(b.begin(), b.end(), b.begin(), [](char x){return isalnum(x) ? std::tolower(x) : 0;});
 	return a==b;
-
 }
 
 class CmyIRK
 {
-   public:
-      CmyIRK(const char *_server, const char *_user, const char *_passwd, const char *_db)
-      {
-         server = _server;
-         user   = _user;
-         passwd = _passwd;
-         db     = _db;
+public:
+	CmyIRK(const char *_server, const char *_user, const char *_passwd, const char *_db)
+	{
+		server = _server;
+		user   = _user;
+		passwd = _passwd;
+		db     = _db;
 
-         try 
-         {
-            driver = get_driver_instance();
-            con = NULL;
-/* - */
-            con = driver -> connect(server, user, passwd);
-            con -> setAutoCommit(0);
-            con->setSchema(db);
-/* - */
-         }
-         catch (sql::SQLException &e) 
-         {
-            cerr << "# ERR: SQLException in " << __FILE__;
-            cerr << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
-            cerr << "# ERR: " << e.what();
-            cerr << " (MySQL error code: " << e.getErrorCode();
-            cerr << ", SQLState: " << e.getSQLState() << " )" << endl;
-            throw MyException("# ERR: Upss ...");
-         }
-      }
-      ~CmyIRK()
-      {
-         if( con!=NULL )
-         {
-            con -> close();
-            delete con;
-         }
-      }
+	try 
+	{
+		driver = get_driver_instance();
+		con = NULL;
 
-	bool CheckPost(BadRecord &record){
-	 stringstream ss;
-         Statement *stmt;
-         ResultSet *res;
-         ss.clear();
-         ss.str("");
-         ss << "SELECT * FROM usos_kody_pocztowe u WHERE u.kod=" << record.KodDigits() << ";";
-         stmt = con -> createStatement();
-         res = stmt -> executeQuery( ss.str().c_str() );
-	 string komunikat="";
+		con = driver -> connect(server, user, passwd);
+		con -> setAutoCommit(0);
+		con->setSchema(db);
 
-	 if(!res->next()){
+	}
+	catch (sql::SQLException &e) 
+	{
+		cerr << "# ERR: SQLException in " << __FILE__;
+		cerr << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		cerr << "# ERR: " << e.what();
+		cerr << " (MySQL error code: " << e.getErrorCode();
+		cerr << ", SQLState: " << e.getSQLState() << " )" << endl;
+		throw MyException("# ERR: Upss ...");
+	}
+}
+~CmyIRK()
+{
+	if( con!=NULL )
+	{
+		con -> close();
+		delete con;
+	}
+}
+
+	bool CheckPost(BadRecord &record);
+	void getInfo();
+
+protected:
+private:
+	string server,user,passwd,db;
+	Driver *driver;
+	Connection *con;
+};
+
+bool CmyIRK::CheckPost(BadRecord &record){
+	string remarks="";
+	stringstream ss;
+        Statement *stmt;
+        ResultSet *res;
+        ss.clear();
+        ss.str("");
+        ss << "SELECT * FROM usos_kody_pocztowe u WHERE u.kod=" << record.KodDigits() << ";";
+        stmt = con -> createStatement();
+        res = stmt -> executeQuery( ss.str().c_str() );
+
+	if(!res->next()){
 		record.InsertError(2); 			// brak kodu pocztowego w bazie USOS
 		return false;
-	 }
-	 else{
+	}
+	else{
 		do{
 			string tmp=res->getString("poczta");
-			 if(SameString(tmp,record.Poczta())){
-			 	record.InsertError(0);			//krotka wpadła do programu, ale wszystko z nią OK
-				return true;
+			if(SameString(tmp,record.Poczta())){
+			 record.InsertError(0);			//krotka wpadła do programu, ale wszystko z nią OK
+         		 delete res;
+         		 delete stmt;
+			 return true;
 			}
 			if(SameString(tmp,record.Miejscowosc())){ 
-				record.InsertError(4); 			// ostrzeżenie -> poczta pasująca do podanego kodu pocztowego jest wpisana w polu miejscowość
-				return true;
+			 record.InsertError(4); // ostrzeżenie -> poczta pasująca do podanego kodu pocztowego jest wpisana w polu miejscowość
+		         delete res;
+         		 delete stmt;			
+			 return true;
 			}
+			remarks += 	"/" + tmp + "/";
 		}while(res->next());
 		record.InsertError(3); //do podanego kodu jest przypisana inna poczta
+		record.InsertRemark("[poczty pod tym kodem:"+remarks + "]");
+		delete res;
+         	delete stmt;
 		return false;
 	 }
-	}
-      
-    void getInfo(){
+}
+
+void CmyIRK::getInfo(){
          stringstream ss;
+	 ofstream data("wyniki");
+	 data << "Nr;Kod;Miejscowosc;Poczta;Błędy;Uwagi"<<endl;
          Statement *stmt;
          ResultSet *res;
+
 
          ss.clear();
          ss.str("");
@@ -261,37 +267,31 @@ class CmyIRK
          {
             BadRecord tmp = 
                 BadRecord(res->getInt("pk"),res->getString("a_kod"),res->getString("a_miejscowosc"), res->getString("a_poczta"));
-		 if(!tmp.IsErr()) CheckPost(tmp); //jeśli nie ma jeszcze po konstrukcji obiektu babola to sprawdza czy poczta jest dobra 
-		cout << tmp << endl;
-		 }
-         delete res;
-         delete stmt;
-      }
+		if(!tmp.IsErr()) CheckPost(tmp); //jeśli nie ma jeszcze po konstrukcji obiektu babola to sprawdza czy poczta jest dobra 
+		//cout << tmp << endl;
+		data << tmp << endl;
 
-   protected:
-   private:
-      string server,user,passwd,db;
-      Driver *driver;
-      Connection *con;
-};
+	}
+        delete res;
+        delete stmt;
+}
 
 int main()
 {
-   LoadDataConn("serverData.txt");
-   CmyIRK *myIRK;
+	LoadDataConn("serverData.txt");
+	CmyIRK *myIRK;
 
-   try
-   {
+	try
+	{
 
-      myIRK = new CmyIRK(serIRK, usrIRK, pwdIRK, dbIRK);
-      myIRK->getInfo();
-      
-
-   }
-   catch(MyException& e)
-   {
-      e.printerrmsg();
-   }
-   delete myIRK;
-   return 0;
+		myIRK = new CmyIRK(serIRK, usrIRK, pwdIRK, dbIRK);
+		myIRK->getInfo();
+     
+	}
+	catch(MyException& e)
+	{
+		e.printerrmsg();
+	}
+	delete myIRK;
+	return 0;
 }
